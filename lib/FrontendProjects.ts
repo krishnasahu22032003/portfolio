2dca2fa39904db8c539194edadd1ca1966cde094
krishna sahu <krishna.sahu.work@gmail.com>
2026-07-15 03:12:01 +0530

    All frontend projects added

--- lib/FrontendProjects.ts
+++ lib/FrontendProjects.ts
@@ -1,4 +1,24 @@
 export const frontendProjects = [
+      {
+    title: "Demon Slayer",
+description:
+  "A cinematic anime-inspired experience featuring seamless video transitions, immersive scroll-driven storytelling, and premium motion design.",
+    image: "/projects/demonslayer.png",
+    github: "https://github.com/krishnasahu22032003/Animated-webapplication",
+    live: "https://animated-webapplication.vercel.app/",
+    stack: ["React", "UI/UX", " React Icons" , "GSAP" , "Tailwind" ,"Vite "],
+    category: "frontend",
+  },
+    {
+    title: "Zylo",
+description:
+  "An intelligent UI platform featuring adaptive design, modern interfaces, and scalable components for next-generation web experiences.",
+    image: "/projects/zylo.png",
+    github: "https://github.com/krishnasahu22032003/Zylo-landing-page",
+    live: "https://zylo-landing-page.vercel.app/",
+    stack: ["React", "Vite","Tailwind" , "GSAP" , "Cloudinary" , "UI/UX"],
+    category: "frontend",
+  },
   {
     title: "Reevid AI",
     description:
@@ -50,33 +70,84 @@ export const frontendProjects = [
     category: "frontend",
   },
   {
-    title: "Astrozyne",
-   description:
-  "A futuristic landing page featuring immersive animations, smooth scroll interactions, and a polished responsive experience.",
-    image: "/projects/astrozyne.png",
-    github: "https://github.com/krishnasahu22032003/Sci-Fi-landing-page",
-    live: "https://sci-fi-landing-page.vercel.app/",
-    stack: ["React", "UI/UX", "Three Js" , "GSAP"],
+    title: "Kivo AI",
+description:
+  "A modern AI SaaS landing page featuring intelligent UI, premium visuals, and a seamless responsive experience.",
+    image: "/projects/kivo.png",
+    github: "https://github.com/krishnasahu22032003/KIVO-landing-page",
+    live: "https://kivo-landing-page.vercel.app/",
+    stack: ["React", "UI/UX", "SplitType" , "GSAP" , "Tailwind"],
     category: "frontend",
   },
   {
-    title: "Astrozyne",
-   description:
-  "A futuristic landing page featuring immersive animations, smooth scroll interactions, and a polished responsive experience.",
-    image: "/projects/astrozyne.png",
-    github: "https://github.com/krishnasahu22032003/Sci-Fi-landing-page",
-    live: "https://sci-fi-landing-page.vercel.app/",
-    stack: ["React", "UI/UX", "Three Js" , "GSAP"],
+    title: "Deadpoolverse",
+description:
+  "A cinematic superhero-inspired experience blending bold visuals, immersive animations, and dynamic motion-driven interactions.",
+    image: "/projects/deadpool.png",
+    github: "https://github.com/krishnasahu22032003/Deadpool-landingpage",
+    live: "https://deadpool-landingpage.vercel.app/",
+    stack: ["React", "UI/UX", "React Icons" , "GSAP" , "Tailwind"],
     category: "frontend",
   },
   {
-    title: "Astrozyne",
-   description:
-  "A futuristic landing page featuring immersive animations, smooth scroll interactions, and a polished responsive experience.",
-    image: "/projects/astrozyne.png",
-    github: "https://github.com/krishnasahu22032003/Sci-Fi-landing-page",
-    live: "https://sci-fi-landing-page.vercel.app/",
-    stack: ["React", "UI/UX", "Three Js" , "GSAP"],
+    title: "NeuroSpark",
+description:
+  "A premium UI/UX experience featuring modern visuals, fluid animations, and responsive components for next-generation web applications.",
+    image: "/projects/deadpool.png",
+    github: "https://github.com/krishnasahu22032003/AI-Landing-page",
+    live: "https://ai-landing-page-m26o.vercel.app/",
+    stack: ["Next Js", "Framer Motion" , "React" , "Tailwind"],
+    category: "frontend",
+  },
+
+  {
+    title: "Athlenix Zone",
+description:
+  "A modern fitness landing page featuring dynamic animations, responsive layouts, and a premium user experience.",
+    image: "/projects/exercise.png",
+    github: "https://github.com/krishnasahu22032003/Exercise-website",
+    live: "https://exercise-website-6xvy.vercel.app/",
+    stack: ["React", "Vite", "GSAP" , "Tailwind"],
+    category: "frontend",
+  },
+  {
+    title: "Globeora",
+description:
+  "A premium travel landing page featuring immersive visuals, fluid animations, and a seamless responsive experience.",
+    image: "/projects/globeora.png",
+    github: "https://github.com/krishnasahu22032003/Travel-website-landing-page",
+    live: "https://travel-website-landing-page-jet.vercel.app/",
+    stack: ["React", "Vite", "Framer Motion" , "GSAP" , "Tailwind"],
+    category: "frontend",
+  },
+  {
+    title: "Versecore",
+description:
+  "A futuristic metaverse landing page featuring immersive animations, interactive visuals, and a premium responsive experience.",
+    image: "/projects/versecore.png",
+    github: "https://github.com/krishnasahu22032003/Versecore-landing-page",
+    live: "https://versecore-landing-page-ifzw.vercel.app/",
+    stack: ["React", "Vite", "Framer Motion" , "GSAP" , "Tailwind"],
+    category: "frontend",
+  },
+  {
+    title: "Truffiq",
+description:
+  "A futuristic ramen experience blending cinematic animations, immersive visuals, and premium responsive design.",
+    image: "/projects/truffiq.png",
+    github: "https://github.com/krishnasahu22032003/Food-landing-page",
+    live: "https://food-landing-page-ruddy-eta.vercel.app/",
+    stack: ["React", "Vite", "Cloudinary" , "GSAP" , "Tailwind"],
+    category: "frontend",
+  },
+  {
+    title: "Vaultory ",
+description:
+  "A modern transaction banking landing page featuring intuitive financial workflows, premium UI, and a seamless responsive experience.",
+    image: "/projects/vaultory.png",
+    github: "https://github.com/krishnasahu22032003/Modern-landing-page",
+    live: "https://modern-landing-page-gamma.vercel.app/",
+    stack: ["React", "Vite","Tailwind"],
     category: "frontend",
   },
 ]
